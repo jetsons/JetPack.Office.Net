2789856ad6170903806626ed60f54d858b691c01
Jetsons <info@github.com>
2020-01-09 00:02:49 +0530

    comment fixes

--- Excel/ExcelImporter.cs
+++ Excel/ExcelImporter.cs
@@ -17,9 +17,10 @@ namespace Jetsons.Excel {
 		/// If the first line is not headers, and you don't supply any columnProps, then the names of the columns are assumed.
 		/// </summary>
 		/// <param name="excelPath">Path of the XLSX file</param>
-		/// <param name="headers">Read the first line of each sheet as the column headers?</param>
+		/// <param name="headers">Read the first line of each sheet as the column headers</param>
 		/// <param name="columnProps">Provide the properties per column, if known</param>
 		/// <param name="onlySheetsNamed">Only returns the sheets with the given name.</param>
+		/// <param name="skipBlankRows">Skips rows where all the cells are blank.</param>
 		/// <returns></returns>
 		public static ExcelResults<T> ImportXlsx<T>(string excelPath, ExcelHeaders headers, List<string> columnProps = null, List<string> onlySheetsNamed = null, bool skipBlankRows = true) {
 
--- Types/OfficeFiles.cs
+++ Types/OfficeFiles.cs
@@ -24,7 +24,7 @@ namespace Jetsons.JetPack
 		/// If the first line is not headers, and you don't supply any columnProps, then the names of the columns are assumed.
 		/// </summary>
 		/// <param name="excelPath">Path of the XLSX file</param>
-		/// <param name="headers">Read the first line of each sheet as the column headers?</param>
+		/// <param name="headers">Read the first line of each sheet as the column headers</param>
 		/// <param name="columnProps">Provide the properties per column, if known</param>
 		/// <param name="onlySheetsNamed">Only returns the sheets with the given name.</param>
 		/// <param name="skipBlankRows">Skips rows where all the cells are blank.</param>
